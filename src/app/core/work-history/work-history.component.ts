e53ddc83cbd61019826c9dfcef408d144df65fb9
Lah Ntumsi <lah_ntumsi2000@yahoo.com>
2024-04-03 12:23:38 -0500

    Updated work history component with an array of work objects

--- src/app/core/work-history/work-history.component.ts
+++ src/app/core/work-history/work-history.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-work-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './work-history.component.html',
   styleUrl: './work-history.component.scss'
 })
 export class WorkHistoryComponent {
+   workHistory =[
+    {
+      startDate: "2020",
+      endDate: "Present",
+      title: "Diagnostic Imaging Instructor",
+      company: "DHA DoD Biomedical Electronics Technician Training School",
+      description: `Proficient in troubleshooting, Systems networking, repair and installation of a vast
+      array of medical and dental diagnostic x-ray systems,and also ; Siemens ultrasound systems
+      interfaced with the Syngo Multi-Modality work place, Zoll/Impact ventilators, Covidien 840
+      Ventilators, Philips Intelivue patient monitoring Systems, Hologic Dimensions Mammography systems.
+      Experienced 3M maintenance manager for Shipboard Electrical, mechanical, Medical and Dental systems.`
+    },
+    {
+      startDate: "2015",
+      endDate: "2020",
+      title: "Biomedical Electronics Technician",
+      company: "US Navy",
+      description: `Proficient in troubleshooting, Systems networking, repair and installation of a vast
+      array of medical and dental diagnostic x-ray systems,and also ; Siemens ultrasound systems
+      interfaced with the Syngo Multi-Modality work place, Zoll/Impact ventilators, Covidien 840
+      Ventilators, Philips Intelivue patient monitoring Systems, Hologic Dimensions Mammography systems.
+      Experienced 3M maintenance manager for Shipboard Electrical, mechanical, Medical and Dental systems.`
+    },
+     {
+      StartDate: "2015",
+      EndDate: "2020",
+      title: "Biomedical Electronics Technician",
+      company: "US Navy",
+      description: `Proficient in troubleshooting, Systems networking, repair and installation of a vast
+      array of medical and dental diagnostic x-ray systems,and also ; Siemens ultrasound systems
+      interfaced with the Syngo Multi-Modality work place, Zoll/Impact ventilators, Covidien 840
+      Ventilators, Philips Intelivue patient monitoring Systems, Hologic Dimensions Mammography systems.
+      Experienced 3M maintenance manager for Shipboard Electrical, mechanical, Medical and Dental systems.`
+    }
+   ]
 
 }
