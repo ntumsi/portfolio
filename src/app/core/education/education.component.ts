e06bd72aacd25cd80ef0613c086984e42ce32883
ntumsi <lah.ntumsi@gmail.com>
2024-08-30 09:48:21 -0500

    modified education, languages, and technologies components to use arrays of json elements and added css to clean up the interfaces

--- src/app/core/education/education.component.ts
+++ src/app/core/education/education.component.ts
@@ -1,13 +1,94 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss'
 })
 export class EducationComponent {
+  education =[
+    {
+      "name": "Post graduate Cloud Computing Certificate",
+      "school": "University of Texas at Austin",
+      "location": "Austin, TX",
+      "date": "October 2020 - March 2021",
+      "gpa": "3.5",
+      "url": "https://www.utexas.edu/",
+      "logo": ""
+    },
+    {
+      "name": "Master's Degree in Information Technology",
+      "school": "Thomas Edison State University",
+      "location": "Trenton, NJ",
+      "date": "July 2016 - June 2018",
+      "gpa": "3.9",
+      "url": "https://www.tesu.edu/",
+      "logo": ""
+    },
+    {
+      "name": "Bachelor's Degree in Applied Science Biomedical Electronics",
+      "school": "Thomas Edison State University",
+      "location": "Trenton, NJ",
+      "date": "November 2010 - September 2015",
+      "gpa": "2.9",
+      "url": "https://www.tesu.edu/",
+      "logo": ""
+    },
+    {
+      "name": "Diploma in Biomedical Electronics",
+      "school": "DoD Medical Education and Training Campus",
+      "location": "San Antonio, TX",
+      "date": "July 2008 - October 2009",
+      "gpa": "3.9",
+      "url": "https://metc.mil",
+      "logo": ""
+    },
+    {
+      "name": "Navy Hospital Corpsman",
+      "school": "Navy Hospital Corps School",
+      "location": "Great Lakes, IL",
+      "date": "August 2006 - November 2006",
+      "gpa": "3.5",
+      "url": "https://www.med.navy.mil/sites/nmcsd/Pages/default.aspx",
+      "logo": ""
+    },
+    {
+      "name": "Airman Apprentice",
+      "school": "Navy Aviation Technical Training Center",
+      "location": "Pensacola, FL",
+      "date": "June 2004 - July 2004",
+      "gpa": "NA",
+      "url": "https://www.bootcamp.navy.mil/",
+      "logo": ""
+    },
+    {
+      "name": "Navy Basic Training",
+      "school": "Navy Recruit Training Command",
+      "location": "Great Lakes, IL",
+      "date": "April 2004 - June 2004",
+      "gpa": "NA",
+      "url": "https://www.bootcamp.navy.mil/",
+      "logo": ""
+    }
+  ]
+
+  certifications =
+    [
+      {
+        "title": "CompTia Security+ Certification",
+        "date": "July 2019",
+        "description": "CompTia Certified security professional"
+      },
+      {
+        "title": "Woz U - Certified Software Developer",
+        "date": "July 2018 - March 2016",
+        "description": "Learned how to build web applications using React and AngularJS. Mobile web development with Ionic/Cordova. Acquired skills to program using Java, build database-driven web applications using Spring MVC, and create richly interactive client-side apps using the JavaScript AngularJS library. Worked 60+ hours weekly on individual and team projects."
+      }
+    ]
+
 
 }
--- src/app/core/languages/languages.component.ts
+++ src/app/core/languages/languages.component.ts
@@ -13,79 +13,77 @@ import { MatCardImage, MatCardModule } from '@angular/material/card';
 export class LanguagesComponent {
   languages = [
     {
-      name: "Python",
-      proficiency: "Intermediate",
-      url: "https://www.python.org/",
-      logo: "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+      "name": "Python",
+      "proficiency": "Intermediate",
+      "url": "https://www.python.org/",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png"
     },
     {
-      name: "JavaScript",
-      proficiency: "Intermediate",
-      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+      "name": "JavaScript",
+      "proficiency": "Intermediate",
+      "url": "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      "logo": "https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-hq-png-1.png"
     },
     {
-      name: "TypeScript",
-      proficiency: "Intermediate",
-      url: "https://www.typescriptlang.org/",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
+      "name": "TypeScript",
+      "proficiency": "Intermediate",
+      "url": "https://www.typescriptlang.org/",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
     },
     {
-      name: "Java",
-      proficiency: "Intermediate",
-      url: "https://www.java.com/en/",
-      logo: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg"
+      "name": "Java",
+      "proficiency": "Intermediate",
+      "url": "https://www.java.com/en/",
+      "logo": "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg"
     },
     {
-      name: "C#",
-      proficiency: "Intermediate",
-      url: "https://docs.microsoft.com/en-us/dotnet/csharp/",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/7/7a/C_Sharp_logo.svg"
+      "name": "C#",
+      "proficiency": "Intermediate",
+      "url": "https://docs.microsoft.com/en-us/dotnet/csharp/",
+      "logo": "https://cdn.prod.website-files.com/6047a9e35e5dc54ac86ddd90/63065002cd563e1cd1cead28_eaadfe64-p-800.png"
     },
     {
-      name: "SQL",
-      proficiency: "Intermediate",
-      url: "https://www.w3schools.com/sql/",
-      logo: "https://upload.wikimedia.org/wikipedia/en/6/68/SQL_logo.png"
+      "name": "SQL",
+      "proficiency": "Intermediate",
+      "url": "https://www.w3schools.com/sql/",
+      "logo": "https://www.mysql.com/common/logos/logo-mysql-170x115.png"
     },
     {
-      name: "HTML",
-      proficiency: "Intermediate",
-      url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+      "name": "HTML",
+      "proficiency": "Intermediate",
+      "url": "https://developer.mozilla.org/en-US/docs/Web/HTML",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
     },
     {
-      name: "CSS",
-      proficiency: "Intermediate",
-      url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-    },
-
-    {
-      name: "SCSS",
-      proficiency: "Intermediate",
-      url: "https://sass-lang.com/",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"
+      "name": "CSS",
+      "proficiency": "Intermediate",
+      "url": "https://developer.mozilla.org/en-US/docs/Web/CSS",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
     },
     {
-      name: "PHP",
-      proficiency: "Beginner",
-      url: "https://www.php.net/",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
-    },
-
-    {
-      name: "PowerShell",
-      proficiency: "Beginner",
-      url: "https://docs.microsoft.com/en-us/powershell/",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/a/af/PowerShell_Core_6.0_icon.png"
+      "name": "SCSS",
+      "proficiency": "Intermediate",
+      "url": "https://sass-lang.com/",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"
     },
     {
-      name: "Bash",
-      proficiency: "Beginner",
-      url: "https://www.gnu.org/software/bash/",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/8/82/Gnu-bash-logo.svg"
+      "name": "PHP",
+      "proficiency": "Beginner",
+      "url": "https://www.php.net/",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
     },
-
+    {
+      "name": "PowerShell",
+      "proficiency": "Beginner",
+      "url": "https://docs.microsoft.com/en-us/powershell/",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/a/af/PowerShell_Core_6.0_icon.png"
+    },
+    {
+      "name": "Bash",
+      "proficiency": "Beginner",
+      "url": "https://www.gnu.org/software/bash/",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/8/82/Gnu-bash-logo.svg"
+    }
   ]
+
 }
